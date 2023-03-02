c630088781063953332cbd2c6b1cf2546893d4fa
matthiasweigt <m.w.weigt@gmail.com>
2023-03-02 18:19:16 +0100

    rendered_tex.dart

---
+++ lib/presentation/widgets/rendered_tex.dart
@@ -0,0 +1,21 @@
+// @author Matthias Weigt 02.03.23
+// All rights reserved ©2023
+
+import 'package:flutter/material.dart';
+
+class RenderedTex extends StatelessWidget {
+  const RenderedTex({super.key, required this.text, required this.textStyle});
+
+  final String text;
+  final TextStyle? textStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text);
+  }
+
+  RichText _compile(String text,TextStyle? textStyle,BuildContext context) {
+    //todo 02.03.23 17:28 von @Matthias to @Yuri: Implement me.
+    throw UnimplementedError();
+  }
+}
